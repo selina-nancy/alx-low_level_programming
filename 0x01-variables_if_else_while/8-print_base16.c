72c840045542992f18545652d91292a684fc6a66
selina-nancy <selinanancy97@gmail.com>
2022-03-11 08:42:43 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,15 +3,17 @@
 /**
 * main - entry point
 *
-* Description: Print lowercase alphabet backwards
+* Description: Print all the digits of base 16 in lowercase
 * Return: 0
 */
 int main(void)
 {
-	char c;
+	char x;
 
-	for (c = 'z'; c >= 'a'; --c)
-		putchar(c);
+	for (x = '0'; x <= '9'; ++x)
+		putchar(x);
+	for (x = 'a'; x <= 'f'; ++x)
+		putchar(x);
 
 	putchar('\n');
 
