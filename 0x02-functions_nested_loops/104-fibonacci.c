3617c40c1479ff0eab7c2b4d692638c6c5dc3586
selina-nancy <selinanancy97@gmail.com>
2022-03-16 03:18:27 -0700

    Finds and prints the first 98 Fibonacci numbers, starting with 1 and 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,48 +1,43 @@
-#include <limits.h>
-#include <math.h>
 #include <stdio.h>
-#define LARGEST 10000000000
+
 /**
- * main - entry point
+ * main - print fibonacci
  *
- * Return: Always 0.
+ * Return: always 0
  */
 
 int main(void)
 {
-	int i;
-	int j;
-	long a_lo = 1;
-	long b_lo = 2;
-	long a_hi = 0;
-	long b_hi = 0;
-	int limit_len = floor(log10(LONG_MAX / 2));
-	long limit = pow(10, limit_len);
+	unsigned long int a, a1, a2, b, b1, b2, c, c1, c2, d, e;
 
-	for (i = 0; i < 98; ++i)
+	a = 1;
+	b = 2;
+	c = a + b;
+
+	printf("%lu, ", a);
+	printf("%lu, ", b);
+	for (d = 3; d < 89; d++)
 	{
-		if (a_hi)
-		{
-			printf("%ld", a_hi);
-			for (j = floor(log10(a_lo)) + 1; j < limit_len; ++j)
-				putchar('0');
-		}
-		printf("%ld", a_lo);
-		b_lo = b_lo + a_lo;
-		a_lo = b_lo - a_lo;
-		a_hi = b_hi - a_hi;
-		b_hi = b_hi + a_hi;
-		if (b_lo >= limit)
-		{
-			b_hi += b_lo / limit;
-			b_lo %= limit;
-			a_hi += a_lo / limit;
-			a_lo %= limit;
-		}
-		if (i < 97)
-			printf(", ");
+		printf("%lu, ", c);
+		a = b;
+		b = c;
+		c = a + b;
 	}
-	putchar('\n');
+	b1 = b / 1000000000;
+	b2 = b % 1000000000;
+	c1 = c / 1000000000;
+	c2 = c % 1000000000;
+	for (e = 89; e < 98; e++)
+	{
+		printf("%lu%lu, ", c1, c2);
+		a1 = b1;
+		a2 = b2;
+		b1 = c1;
+		b2 = c2;
+		c1 = a1 + b1 + ((a2 + b2) / 1000000000);
+		c2 = (a2 + b2) % 1000000000;
+	}
+	printf("%lu%lu\n", c1, c2);
 
-	return (0);
+		return (0);
 }
