7d1e535fd0be6229fb12314fd851c39d20874706
selina-nancy <selinanancy97@gmail.com>
2022-03-24 11:45:41 -0700

    Concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,16 @@
-#incude "main.h"
+#include "main.h"
 
 /**
- * _strcat - concatenate two strings
- * @dest: the string to append to
- * @src: the string to append
+ *  _strcat - concatenate two strings
+ *  @dest: the string to append to
+ *  @src: the string to append
  *
- * Description: This function appends the string pointed to by src to the
- * string pointed to by dest, overwriting the null byte at the end of dest.
+ *  Description: This function appends the string pointed to by src to the
+ *  string pointed to by dest, overwriting the null byte at the end of dest.
  *
- * Return: a pointer to the destination string dest
+ *  Return: a pointer to the destination string dest
  */
+
 char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
@@ -21,8 +22,8 @@ char *_strcat(char *dest, char *src)
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		j++;
 		i++;
+		j++;
 	}
 	dest[i] = '\0';
 	return (dest);
